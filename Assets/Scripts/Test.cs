281c855553205c333f83315e06e66f75bd1ba8a1
Reavenk <Reavenk@gmail.com>
2020-12-22 22:34:11 -0600

    Added working test

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -7,10 +7,16 @@ using PxPre.Tree;
 public class Test : MonoBehaviour
 {
     public PxPre.Tree.Tree tree;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        PxPre.Tree.Node firstEntry = tree.AddNode("First entry", null);
+        PxPre.Tree.Node secondEntry = tree.AddNode("Second entry", null);
+        PxPre.Tree.Node thirdEntry = tree.AddNode("Third entry", null);
+
+        PxPre.Tree.Node firstChild1 = tree.AddNode("First child in first child", firstEntry);
+        PxPre.Tree.Node firstChild2 = tree.AddNode("second one", firstEntry);
     }
 
     // Update is called once per frame
